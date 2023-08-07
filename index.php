330db4d8d8a48f9569eac6adf0e002ce407e72ef
mostafarabie22 <137173362+mostafarabie22@users.noreply.github.com>
2023-08-07 18:40:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$name=$_POST['full_name'];
+$email=$_POST['email_address'];
+$company=$_POST['company_name'];
+$notes=$_POST['message']
+
+$connection_form1= mysqli_connect('localhost','root','root','contact_form_info')
+if(isset($_POST['submit'])){
+
+}
+
+
+
+
+?>
